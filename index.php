edfd24b404a8bb0dec45466bd238f505107e92a6
shosa <kishosa@me.com>
2024-05-31 19:07:07 +0200

    Sistemato Footer in Index

--- index.php
+++ index.php
@@ -378,18 +378,12 @@ try {
                 <!-- End of Main Content -->
 
                 <!-- Footer -->
-                <footer class="sticky-footer bg-white">
-                    <div class="container my-auto">
-                        <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Your Website 2021</span>
-                        </div>
-                    </div>
-                </footer>
+
                 <!-- End of Footer -->
 
             </div>
             <!-- End of Content Wrapper -->
-
+            <?php include (BASE_PATH . "/components/footer.php"); ?>
         </div>
         <!-- End of Page Wrapper -->
 
