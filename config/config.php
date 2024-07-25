24bf6e6d4e66195a70d8943a29b50718d059f44f
shosa <kishosa@me.com>
2024-07-25 12:51:09 +0200

    Attivata istanza Debug

--- config/config.php
+++ config/config.php
@@ -6,22 +6,4 @@ ini_set('display_errors', 'On');
 require ("url.php");
 
 require ("database.php");
-/*
-define('DB_HOST', "localhost");
-define('DB_USER', "root");
-define('DB_PASSWORD', "");
-define('DB_NAME', "my_webgre");
 
-function getDbInstance()
-{
-	$host = 'localhost';
-	$dbname = 'my_webgre';
-	$username = 'root';
-	$password = '';
-
-	$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	return $pdo;
-}
-*/
-$debug = true;
