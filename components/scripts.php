333e5ee1dd4a1c1a5765df9e1d6664c9b18bf345
Stefano Solidoro <kishosa@me.com>
2024-07-31 16:39:58 +0200

    Aggiunto AUTOUPDATE

--- components/scripts.php
+++ components/scripts.php
@@ -1,10 +1,10 @@
-<script src="<?php BASE_PATH ?>/vendor/jquery/jquery.min.js"></script>
-<script src="<?php BASE_PATH ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="<?php BASE_PATH ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="<?php BASE_PATH ?>/js/sb-admin-2.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/js/sb-admin-2.min.js"></script>
 
-<script src="<?php BASE_PATH ?>/vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/vendor/jquery/jquery.min.js"></script>
