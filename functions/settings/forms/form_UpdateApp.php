b217df260477552a92441661290e87843e808358
Stefano Solidoro <kishosa@me.com>
2024-07-31 16:34:51 +0200

    Aggiunto AUTOUPDATE

--- functions/settings/forms/form_UpdateApp.php
+++ functions/settings/forms/form_UpdateApp.php
@@ -1,11 +1,11 @@
 <div>
     <p>Clicca il bottone qui sotto per aggiornare l'app alla versione più recente.</p>
     <button id="updateAppBtn" class="btn btn-success p-2">Aggiorna</button>
-    <div class="mt-2">
-        <label for="progressBar">Aggiornamento:</label>
+    <div class="mt-4 p-4 border rounded shadow-sm bg-light">
+        <label for="progressBar">Stato:</label>
         <progress id="progressBar" value="0" max="100" style="width: 100%;"></progress>
-        <span id="progressText">In attesa di avvio...</span>
+        <span id="progressText" class="font-italic">In attesa di avvio...</span>
+        <pre id="updateLog" class="mt-2" style="display: none; background-color: black; color:lime; padding: 10px; border: 1px solid #e0e0e0; max-height: 300px; overflow-y: auto;"></pre>
     </div>
-    <pre id="updateLog" class="mt-2" style="display: none; background-color: black; color:lime; padding: 10px; border: 1px solid #e0e0e0; max-height: 300px; overflow-y: auto;"></pre>
 </div>
 
