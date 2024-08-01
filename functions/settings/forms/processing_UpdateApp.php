0644d79b764b6ea3052c450eec809b198031a41a
Stefano Solidoro <kishosa@me.com>
2024-08-01 12:42:07 +0200

    Aggiunto Log su Aggiornamento App

--- functions/settings/forms/processing_UpdateApp.php
+++ functions/settings/forms/processing_UpdateApp.php
@@ -1,25 +1,26 @@
 <?php
 require_once '../../../config/config.php';
+require_once BASE_PATH . '/utils/log_utils.php';
 header('Content-Type: text/plain');  // Set header to plain text for better logging output
 
 $pdo = getDbInstance();
 $repoOwner = 'shosa';
 $repoName = 'webgre_v2';
 $branch = "main";
-$tempDir = 'temp_update'; 
+$tempDir = 'temp_update';
 $baseDir = BASE_PATH;  // Usa BASE_PATH per puntare alla root della web app
 
 echo "Log aggiornamento:\n";
 
 // URL per ottenere l'ultima release
 $zipUrl = "https://api.github.com/repos/$repoOwner/$repoName/zipball/$branch";
-
+logActivity($_SESSION['user_id'], 'APP', 'AGGIORNAMENTO', 'Lanciato aggiornamento', "", '');
 // Crea un contesto di stream per includere l'header di autenticazione
 $accessToken = $pdo->query("SELECT value FROM settings WHERE item = 'github_token'")->fetchColumn();
 $options = [
     'http' => [
         'header' => "User-Agent: PHP\r\n" .
-                    "Authorization: token $accessToken\r\n"
+            "Authorization: token $accessToken\r\n"
     ]
 ];
 
@@ -46,7 +47,8 @@ if ($zip->open($zipFile) === TRUE) {
 }
 
 // Funzione per confrontare e aggiornare i file
-function updateFiles($source, $dest) {
+function updateFiles($source, $dest)
+{
     $dir = opendir($source);
     while (($file = readdir($dir)) !== false) {
         if ($file != '.' && $file != '..') {
@@ -82,7 +84,8 @@ $extractedDir = glob($tempDir . '/*', GLOB_ONLYDIR)[0];  // Trova la directory e
 updateFiles($extractedDir, $baseDir);
 
 // Rimuovi la directory temporanea e il file zip scaricato
-function rrmdir($dir) {
+function rrmdir($dir)
+{
     $success = true;  // Variabile per tenere traccia del successo
     foreach (glob($dir . '/{,.}[!.,!..]*', GLOB_MARK | GLOB_BRACE) as $file) {
         if (is_dir($file)) {
