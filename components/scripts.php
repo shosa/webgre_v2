2af53304d42bba6c5867bf7a3a04fd2eadc1c64d
Stefano Solidoro <kishosa@me.com>
2024-09-06 09:20:38 +0200

    PWA

--- components/scripts.php
+++ components/scripts.php
@@ -8,3 +8,16 @@
 <script src="<?php echo BASE_URL; ?>/js/sb-admin-2.min.js"></script>
 
 <script src="<?php echo BASE_URL; ?>/vendor/jquery/jquery.min.js"></script>
+<script>
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('<?php echo BASE_URL; ?>/service-worker.js')
+                .then((registration) => {
+                    console.log('Service Worker registered with scope:', registration.scope);
+                })
+                .catch((error) => {
+                    console.error('Service Worker registration failed:', error);
+                });
+        });
+    }
+</script>
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'myMicaApp';
+const urlsToCache = [
+  '/',
+  '/css/sb-admin-2.css',
+  '/vendor/fontawesome-free/css/all.min.css',
+  '/vendor/datatables/dataTables.bootstrap4.min.css',
+  '/img/logo.png',
+  '/img/logoMini.png',
+  '/img/roundLogo.png'
+    // Aggiungi altre risorse che vuoi cacheare
+];
+
+// Installa il service worker e cachea le risorse
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Gestisce le richieste di rete
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - ritorna la risposta dal cache
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+    })
+  );
+});
