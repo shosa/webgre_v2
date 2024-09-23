c7aca118cf016949c927c8b1f8ae269222e47d85
Stefano Solidoro <kishosa@me.com>
2024-09-23 11:44:32 +0200

    bugfix

--- functions/settings/forms/form_UpdateApp.php
+++ functions/settings/forms/form_UpdateApp.php
@@ -10,15 +10,30 @@
         vertical-align: middle;
         margin-left: 10px;
     }
+
     @keyframes spin {
         0% {
             transform: rotate(0deg);
         }
+
         100% {
             transform: rotate(360deg);
         }
     }
 </style>
+<?php
+require_once '../../../config/config.php';
+$conn = getDbInstance();
+function getToken($conn, $item)
+{
+    $sql = "SELECT value FROM settings WHERE item = :item";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':item', $item);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+$token = getToken($conn, 'github_token');
+?>
 <div>
     <p>Clicca il bottone per aggiornare l'app alla versione più recente.</p>
     <button id="updateAppBtn" class="btn btn-success ml-2">Aggiorna</button>
@@ -29,5 +44,24 @@
         <span id="progressText" class="font-italic">In attesa dell'avvio da parte del Utente...</span>
         <pre id="updateLog" class="mt-2 rounded"
             style="display: none; background-color: black; color:lime; padding: 10px; border: 1px solid #e0e0e0; max-height: 300px; overflow-y: auto;"></pre>
+    </div>
+    <div class="accordion p-2" id="accordionToken">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h5 class="mb-0">
+                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                        aria-expanded="true" aria-controls="collapseOne">
+                        Token GitHub
+                    </button>
+                </h5>
+            </div>
+
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionToken">
+                <div class="card-body">
+                    <input type="text" class="form-control" value="<?= htmlspecialchars($token['value']) ?>"
+                        data-field="token" readonly>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
