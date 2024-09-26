0b356109626ef03a60359a033f22465f3d04c9e4
Stefano Solidoro <kishosa@me.com>
2024-09-26 11:40:03 +0200

    Fixed Delete User Tool

--- functions/users/delete_user.php
+++ functions/users/delete_user.php
@@ -3,12 +3,19 @@ require_once '../../config/config.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_POST['id'];
     $pdo = getDbInstance();
-    $stmt = $pdo->prepare("DELETE FROM utenti WHERE id = :id");
+    $stmt = $pdo->prepare("DELETE FROM activity_log WHERE user_id = :id");
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     if ($stmt->execute()) {
-        echo "Utente eliminato con successo";
+        $stmt2 = $pdo->prepare("DELETE FROM utenti WHERE id = :id");
+        $stmt2->bindParam(':id', $id, PDO::PARAM_INT);
+        if ($stmt2->execute()) {
+            echo "Utente eliminato con successo";
+        } else {
+            echo "Errore durante l'eliminazione dell'utente";
+        }
     } else {
         echo "Errore durante l'eliminazione dell'utente";
     }
+
 }
 ?>
