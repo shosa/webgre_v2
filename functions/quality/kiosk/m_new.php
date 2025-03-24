09be04861b0ceacc7fbba0b094352517328eecd8
Stefano Solidoro <kishosa@me.com>
2025-03-24 08:42:42 +0100

    update kiosk mode

--- functions/quality/kiosk/m_new.php
+++ functions/quality/kiosk/m_new.php
@@ -116,7 +116,7 @@ require_once BASE_PATH . '/components/header_kiosk.php';
             if (xhr.readyState === 4 && xhr.status === 200) {
                 var response = JSON.parse(xhr.responseText);
                 if (response.exists) {
-                    window.location.href = 'add?cartellino=' + encodeURIComponent(cartellino);
+                    window.location.href = 'm_add?cartellino=' + encodeURIComponent(cartellino);
                 } else {
                     Swal.fire({
                         icon: 'error',
