cca84f50121017321df6f75f541835b0f317a5ef
shosa <kishosa@me.com>
2025-03-26 21:36:25 +0100

    test filemanager

--- functions/file.php
+++ functions/file.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../../config/config.php';
+require_once '../config/config.php';
 require_once BASE_PATH . '/components/auth_validate.php';
 require_once BASE_PATH . '/components/header.php';
 
