13d5952e9e7d27757982ca8c19314b5e28e7b3d8
Stefano Solidoro <kishosa@me.com>
2025-05-19 15:12:35 +0200

    SESSION migliorato controllo su auth_validate

--- components/auth_validate.php
+++ components/auth_validate.php
@@ -3,13 +3,13 @@
 require_once(dirname(__DIR__) . '/config/url.php');
 
 // Continua con il resto del codice di autenticazione
-if (!isset($_SESSION['user_logged_in']) && basename($_SERVER['PHP_SELF']) !== 'login.php') {
-    header('Location: ' . BASE_URL . '/login.php');
+if (!isset($_SESSION['user_logged_in']) && basename($_SERVER['PHP_SELF']) !== 'login') {
+    header('Location: ' . BASE_URL . '/login');
     exit;
-}
+
 // Continua con il resto del codice di autenticazione
-if (!isset($_SESSION['user_id']) && basename($_SERVER['PHP_SELF']) !== 'login.php') {
-    header('Location: ' . BASE_URL . '/login.php');
+if (!isset($_SESSION['user_id']) && basename($_SERVER['PHP_SELF']) !== 'login') {
+    header('Location: ' . BASE_URL . '/login');
     exit;
 }
 ?>
