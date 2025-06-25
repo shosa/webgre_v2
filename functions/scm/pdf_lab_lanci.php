bba0a6b3d4c0bea61c47c5fe24a4d0cbeda0ffe6
Stefano Solidoro <kishosa@me.com>
2025-06-25 08:37:18 +0200

    SCM report pdf

--- functions/scm/pdf_lab_lanci.php
+++ functions/scm/pdf_lab_lanci.php
@@ -33,7 +33,7 @@ try {
         FROM scm_lanci l
         WHERE l.laboratorio_id = ? 
         AND l.stato_generale = 'IN_LAVORAZIONE'
-        ORDER BY l.data_lancio ASC
+        ORDER BY l.data_lancio DESC
     ");
     $stmt->execute([$laboratorio_id]);
     $lanci = $stmt->fetchAll(PDO::FETCH_ASSOC);
